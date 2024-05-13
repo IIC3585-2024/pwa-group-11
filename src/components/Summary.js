2410894dd308425ef02bf70d05be381640d4be5b
Sofia Pavlovic <spavlovic@uc.cl>
2024-05-13 09:02:04 -0400

    fix saldar deuda

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -18,14 +18,14 @@ function Summary() {
     const fechaFormateada = `${año}-${mes}-${dia}`;
     const deudor = users.filter(user => user.id === parseFloat(debt[0]))[0]
     const acreedor = users.filter(user => user.id === parseFloat(debt[1]))[0]
-    post("transactions", { transactionId: id,  name: "Pago Deuda", date: fechaFormateada, amount: debt[2], paidBy: deudor.id, userId: deudor.id })
+    post("transactions", { transactionId: id,  name: "Pago Deuda", date: fechaFormateada, amount: 2*debt[2], paidBy: deudor.id, userId: deudor.id })
     .then(() => {
       console.log("exito");
     })
     .catch((error) => {
       console.error("Error:", error);
     });
-    post("transactions", { transactionId: id,  name: "Pago Deuda", date: fechaFormateada, amount: debt[2], paidBy: deudor.id, userId: acreedor.id })
+    post("transactions", { transactionId: id,  name: "Pago Deuda", date: fechaFormateada, amount: 0, paidBy: deudor.id, userId: acreedor.id })
     .then(() => {
       console.log("exito");
     })
